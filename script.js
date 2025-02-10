d811614a81dbecd15f045135b8fadd6f7df84617
Sean Lim <144643739+bobblejotcake@users.noreply.github.com>
2025-02-10 20:32:58 +1000

    working nav open and close

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-const collection = document.getElementsByClassName("clicknav");
-console.log(collection);
+var collection = document.getElementsByClassName("clicknav");
+
+var showNavMobile = function() {
+    var nav = document.getElementById("smallnav");
+    if (nav.style.display == "none"){
+        nav.style.display = "flex"
+    }
+    else{
+        nav.style.display = "none"
+    }
+
+};
+
+Array.from(collection).forEach(function(element) {
+    element.addEventListener('click', showNavMobile);
+  });
